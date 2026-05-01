15b2bf9d3d58d0e7b8d472f96ca21b401e26ab1c
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2026-05-01 12:13:48 +0100

    Add project files.

---
+++ Y10_RaspberryPi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Device.Gpio;
+using System.Threading;
+
+namespace Y10_RaspberryPi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GpioController controller = new GpioController(PinNumberingScheme.Board);
+            int pin = 36;
+            int lightTime = 500;
+
+            for (int i = 0; i < 10; i++)
+            {
+                controller.OpenPin(pin, PinMode.Output);
+
+                Console.WriteLine("On "); // Output to console for debugging 
+                controller.Write(pin, PinValue.High);
+
+                Thread.Sleep(lightTime);
+                Console.WriteLine("Off ");
+                controller.Write(pin, PinValue.Low);
+                controller.ClosePin(pin);
+                Thread.Sleep(lightTime);
+            }
+        }
+    }
+}
